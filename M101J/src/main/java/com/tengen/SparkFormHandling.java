671cf52e76d2de415556df4296c33e7e99a962df
zzzsmiller <smiler@smiler-VirtualBox.(none)>
2013-03-19 00:38:36 +0400

    spark form and post request

--- M101J/src/main/java/com/tengen/SparkFormHandling.java
+++ M101J/src/main/java/com/tengen/SparkFormHandling.java
@@ -1,5 +1,18 @@
 package com.tengen;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: smiler
@@ -8,4 +21,38 @@ package com.tengen;
  * To change this template use File | Settings | File Templates.
  */
 public class SparkFormHandling {
+    public static void main(String[] args) {
+        Spark.get(new Route("/fruits") {
+            @Override
+            public Object handle(Request request, Response response) {
+                StringWriter writer = new StringWriter();
+                Map<String, Object> fruits = new HashMap<String, Object>();
+                fruits.put("fruits", Arrays.asList("apple", "banana", "orange", "pie", "peach"));
+
+                Configuration configuration = new Configuration();
+                configuration.setClassForTemplateLoading(SparkFormHandling.class, "/");
+                try {
+                    Template template = configuration.getTemplate("fruit_picker.ftl");
+                    template.process(fruits, writer);
+                } catch (Exception e) {
+                    halt(100);
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+
+                return writer;
+            }
+        });
+
+        Spark.post(new Route("/favourite_fruit") {
+            @Override
+            public Object handle(Request request, Response response) {
+                String fruit = request.queryParams("fruit");
+                if (fruit == null) {
+                    return "You didn't choose anything, be careful.";
+                } else {
+                    return String.format("You chose %s. Great chose!", fruit);
+                }
+            }
+        });
+    }
 }
--- M101J/src/main/resources/fruit_picker.ftl
+++ M101J/src/main/resources/fruit_picker.ftl
@@ -1,8 +1,18 @@
 <html>
 <head>
-    <title></title>
+    <title>Favourite fruit</title>
 </head>
 <body>
+<h2>Choose your favourite fruit.</h2>
+<form action="/favourite_fruit" method="POST">
+    <#list fruits as fruit>
+        <p>
+            <input type="radio" name="fruit" value="${fruit}">${fruit} </input>
+        </p>
+    </#list>
+
+    <input type="submit" value="Choose">
+</form>
 
 </body>
 </html>
