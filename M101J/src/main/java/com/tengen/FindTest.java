94682376ad908adaafe4a8dced3434a22339550e
zzzsmiller <noname@email.no>
2013-04-03 17:15:10 +0400

    add test use of find

---
+++ M101J/src/main/java/com/tengen/FindTest.java
@@ -0,0 +1,11 @@
+package com.tengen;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: musatov
+ * Date: 4/3/13
+ * Time: 4:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FindTest {
+}
